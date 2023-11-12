e2c9ec90465461c98248bc714c72cb9eba9e1cf5
Charlie-83 <11279546+Charlie-83@users.noreply.github.com>
2023-11-12 07:33:06 +0000

    [nvim] Set smartcase for search

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,23 +1,23 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system {
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    }
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(require("plugins"), require("plugins.configs.lazy_nvim"))
+require("lazy").setup(require "plugins", require "plugins.configs.lazy_nvim")
 
-vim.cmd("colorscheme catppuccin")
+vim.cmd "colorscheme catppuccin"
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
-require("mappings")
+require "mappings"
 
 vim.o.number = true
 vim.o.tabstop = 4
@@ -25,4 +25,7 @@ vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 
-os.execute("git -C /home/charlie/notes pull")
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+os.execute "git -C /home/charlie/notes pull"
