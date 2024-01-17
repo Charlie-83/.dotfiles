03b29e9af4980bc573ca573d30e9c39ea1df6727
Charlie-83 <11279546+Charlie-83@users.noreply.github.com>
2024-01-17 09:24:32 +0000

    [nvim] Add #pragma once to hpp files

--- nvim/lua/autocommands/init.lua
+++ nvim/lua/autocommands/init.lua
@@ -1,1 +1,10 @@
 require "autocommands.ft_mapping"
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+    pattern = "*.hpp",
+    callback = function()
+        if vim.api.nvim_buf_get_lines(0, 0, 1, false) ~= "#pragma once" then
+            vim.api.nvim_buf_set_lines(0, 0, 0, false, { "#pragma once" })
+        end
+    end,
+})
