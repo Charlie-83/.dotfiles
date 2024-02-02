b464de0afe86b24cc694864acdbd32ebb60c0e69
Charlie-83 <11279546+Charlie-83@users.noreply.github.com>
2024-02-02 08:06:41 +0000

    [nvim][null-ls] Use python packages from venv

--- nvim/lua/plugins/configs/null-ls.lua
+++ nvim/lua/plugins/configs/null-ls.lua
@@ -1,11 +1,11 @@
 local null_ls = require("null-ls")
 local opts = {
     sources = {
-        null_ls.builtins.diagnostics.mypy,
-        null_ls.builtins.diagnostics.pylint,
+        null_ls.builtins.diagnostics.mypy.with({ prefer_local = ".venv/bin" }),
+        null_ls.builtins.diagnostics.pylint.with({ prefer_local = ".venv/bin" }),
+        null_ls.builtins.formatting.black.with({ prefer_local = ".venv/bin" }),
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.dart_format,
-        null_ls.builtins.formatting.black,
     },
 }
 return opts
