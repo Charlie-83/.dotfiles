95fb9ee949f5cb46fdfdcd6c01ad8ee339c19fc4
Charlie-83 <11279546+Charlie-83@users.noreply.github.com>
2024-06-17 09:09:38 +0100

    [nvim][lsp] Add clang-format for glsl

--- nvim/lua/plugins/configs/null-ls.lua
+++ nvim/lua/plugins/configs/null-ls.lua
@@ -4,6 +4,9 @@ local opts = {
         null_ls.builtins.diagnostics.mypy.with({ prefer_local = ".venv/bin" }),
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.dart_format,
+        null_ls.builtins.formatting.clang_format.with({
+            filetypes = { "glsl" },
+        }),
         require("none-ls.formatting.jq"),
     },
 }
