450c4408c9bbd9b7e2d0566fe636d452243315b1
Charlie-83 <11279546+Charlie-83@users.noreply.github.com>
2024-06-30 21:58:43 +0100

    [tmux] Remove

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ done
 
 mkdir -p ~/.config
 
-for f in fish i3 kitty nvim polybar systemd tmux yay Xmodmap; do
+for f in fish i3 kitty nvim polybar yay Xmodmap; do
     rm -f ~/.config/${f}
     ln -s ~/.dotfiles/${f} ~/.config/${f}
 done
