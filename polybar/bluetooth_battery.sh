7e34540774bbd1336b951dbfc41684607eb64d33
Charlie-83 <11279546+Charlie-83@users.noreply.github.com>
2025-05-16 19:44:09 +0100

    [polybar] Delete unused script

--- polybar/bluetooth_battery.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-#address=$(bluetoothctl devices Paired | grep charliesweep | cut -d ' ' -f 2)
-#echo "KB $(bluetoothctl info $addr | grep "Battery Percentage" | awk -F'[()]' '{print $2}')%"
-python ~/.dotfiles/polybar/zmk_split_battery_level_linux.py
