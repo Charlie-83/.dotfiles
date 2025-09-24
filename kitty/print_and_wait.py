da94f9b53387985096bd48e63168608984f824a3
Charlie-83 <11279546+Charlie-83@users.noreply.github.com>
2025-09-24 08:33:13 +0100

    [kitty] Improve swap overlay kitten

--- kitty/print_and_wait.py
+++ kitty/print_and_wait.py
@@ -1,4 +1,13 @@
 import sys
 import time
-print(sys.argv[1])
+import shutil
+from art import text2art
+
+text = text2art(sys.argv[1])
+width = shutil.get_terminal_size(fallback=(80, 20)).columns
+left_padding = max((width - text.find("\n")) // 2, 0)
+text = text.replace("\n", "\n" + " " * left_padding)
+height = shutil.get_terminal_size(fallback=(80, 20)).lines
+top_padding = height // 2
+print("\n" * top_padding + " " * left_padding + text)
 time.sleep(float(sys.argv[2]))
--- kitty/swap_overlay.py
+++ kitty/swap_overlay.py
@@ -1,6 +1,5 @@
 from kitty.boss import Boss
 from kittens.tui.handler import result_handler
-from kitty.boss import Boss
 
 
 def main(args: list[str]) -> str:
@@ -11,9 +10,12 @@ def main(args: list[str]) -> str:
 def handle_result(
     args: list[str], answer: str, target_window_id: int, boss: Boss
 ) -> None:
+    w = boss.window_id_map.get(target_window_id)
     tab = boss.active_tab
     if args[1] == "next":
         group = list(tab.windows.windows_in_group_of(tab.active_window))
+        if len(group) == 1:
+            return
         new_window = group[0]
         tab.move_window_to_top_of_group(new_window)
         group_ids = [w.id for w in group]
@@ -22,13 +24,13 @@ def handle_result(
             "--type=overlay",
             "python",
             "/home/charlie/.config/kitty/print_and_wait.py",
-            f"[{", ".join(
-                f"[{i}]" if i == highlight else str(i) for i in range(len(group_ids))
-            )}]",
-            "0.5",
+            f"{highlight + 1}/{len(group_ids)}",
+            "0.6",
         )
     elif args[1] == "make":
         window = tab.active_window
-        boss.call_remote_control(window, ("launch", "--type=overlay-main", "--cwd=current"))
+        boss.call_remote_control(
+            window, ("launch", "--type=overlay-main", "--cwd=current")
+        )
     else:
         raise ValueError(f"Unknown command '{args[1]}'")
